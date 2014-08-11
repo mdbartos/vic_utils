cadd642249e12a270b35bd23cac0edbaca104577
mdbartos <matthew.d.bartos@gmail.com>
2014-08-11 11:29:34 -0700

    modified:   data_prep/usgs_parser.py

--- data_prep/usgs_parser.py
+++ data_prep/usgs_parser.py
@@ -109,3 +109,51 @@ for h in huc_ix.keys():
 	get_usgs(h)
 	
 pickle.dump( temp_d, open( "basin_temps_all.p", "wb" ) )
+
+################ VALIDATION DATASET ##########################
+
+valid_basins = {}
+
+for fn in os.listdir('.'):
+	sp = fn.split('.')
+	t = pd.read_csv(fn)
+	for i, k in t.iterrows():
+		valid_basins.update({sp : {}})
+		valid_basins[sp].update({k['SITE_NO'] : { 'nm' : k['STATION_NAME'], 'lat' : k['LAT_SITE'], 'lon' : k['LON_SITE'] }})
+	
+
+q_d = {}
+		
+def get_validation_q(basin):
+	q_d.update({basin : {}})
+	for i in valid_basins[basin].keys():
+		string_op = ("http://waterdata.usgs.gov/nwis/dv?referred_module=sw&search_site_no=%s" % (i))
+		string_ed = "&search_site_no_match_type=exact&site_tp_cd=OC&site_tp_cd=OC-CO&site_tp_cd=ES&site_tp_cd=LK&site_tp_cd=ST&site_tp_cd=ST-CA&site_tp_cd=ST-DCH&site_tp_cd=ST-TS&index_pmcode_00060=1&group_key=NONE&sitefile_output_format=html_table&column_name=agency_cd&column_name=site_no&column_name=station_nm&range_selection=date_range&begin_date=1900-01-01&end_date=2014-07-15&format=rdb&date_format=YYYY-MM-DD&rdb_compression=value&list_of_search_criteria=search_site_no%2Csite_tp_cd%2Crealtime_parameter_selection"
+		conn_str = string_op + string_ed
+		conn = url.urlopen(conn_str)
+		r = conn.readlines()
+		if len(r) < 10:
+			print "no data"
+			continue
+		else:
+			st_i = None
+			for x, line in enumerate(r):
+				if "No sites" in line[0]:
+					print "No sites found in %s" % (x)
+					break
+				elif 'agency_cd' in line:
+					st_i = x
+			
+			if st_i != None:
+				raw_li = r[st_i:]
+				raw_str = ''.join(raw_li)
+				st_input = StringIO(raw_str)
+				t = pd.read_table(st_input)
+				t = t[1:]
+				print "found data"
+				q_d[basin].update({i : t})
+				
+for h in valid_basins.keys():
+	get_validation_q(h)
+	
+pickle.dump( temp_d, open( "validation_streamflows.p", "wb" ) )
