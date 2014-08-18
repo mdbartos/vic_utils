456456e039b73301e74362e866244656650fb6d8
mdbartos <matthew.d.bartos@gmail.com>
2014-08-18 16:16:12 -0700

    modified:   rbm/rbm_post.py

--- rbm/rbm_post.py
+++ rbm/rbm_post.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+import pickle
 
 class rbm_post():
 
@@ -11,7 +12,8 @@ class rbm_post():
 		self.st_d.update({'st_rc' : self.st_rc, 'st_op' : self.st_op})
 		self.rbm_path = rbm_path
 		self.spat_d = {}
-		self.diff_d = {}
+		self.cell_d = {}
+		self.ll_d = {}
 
 	def make_spat_d(self):
 		for fn in os.listdir(self.rbm_path):
@@ -19,34 +21,55 @@ class rbm_post():
 			self.spat_d.update({fn : spat})
 
 	def make_diff(self):
-		temp_diff_d = {}
+		temp_cell_d = {}
+		temp_ll_d = {}
 		for p in self.st_d.keys():
-			temp_diff_d.update({p : {}})
+			temp_cell_d.update({p : {}})
+			temp_ll_d.update({p : {}})
 		for tech in self.st_d.keys():
 			for i in self.st_d[tech].keys():
 				if i in self.spat_d.keys():
-					j_d = {}
+					jcell_d = {}
+					jll_d = {}
 			#		print os.getcwd()
 					for k in self.st_d[tech][i].values:
-						fn_d = {}
-						j_d.update({k[1] : None})
+						cell_d = {}
+						ll_d = {}
+						jcell_d.update({k[1] : None})
+						jll_d.update({k[1] : None})
 			#			print j_d
 						stn_ll = k[2]
 						for spat_ll in self.spat_d[i].values:
 			#				fn_d = {}
 							diff = ((spat_ll[4] - stn_ll[0])**2 + (spat_ll[5] - stn_ll[1])**2)**0.5
-							fn_d.update({str(tuple([spat_ll[4], spat_ll[5]])) : diff})
-						cell = min(fn_d, key=fn_d.get)
+							cell_d.update({spat_ll[1] : diff})	
+							ll_d.update({str(tuple([spat_ll[4], spat_ll[5]])) : diff})
+						cell = min(cell_d, key=cell_d.get)
+						ll = min(ll_d, key=ll_d.get)
 						print tech, i, k[1], cell
-						mi = fn_d[cell]
-						j_d[k[1]] = cell		
+						mi = cell_d[cell]
+						jcell_d[k[1]] = cell
+						jll_d[k[1]] = ll
 			#		print j_d.items()
-					temp_diff_d[tech].update({i : j_d})
-				self.diff_d[tech] = temp_diff_d[tech]
+					temp_cell_d[tech].update({i : jcell_d})
+					temp_ll_d[tech].update({i : jll_d})
+				self.cell_d[tech] = temp_cell_d[tech]
+				self.ll_d[tech] = temp_ll_d[tech]
 
-	def read_temp():
-		for i in st_rc.items():
-			rpath = rbmpath + '/' + i
+	def read_temp(self, scen):
+		for tech in self.st_d.keys():
+			for i, k in self.cell_d[tech].items():
+				rpath = self.rbm_path + '/' + i
+				with open('%s/%s.Temp' % (rpath, scen), 'rb') as tempfile:
+					for cell in self.cell_d[tech][i].values():
+						for line in tempfile:
+							sp = line.split()
+							if sp[3] == str(cell).split('.')[0]: 
+								print line
+	
+	def get_atmos(self):
+		for i, k in self.cell_d[tech]:
+			pass
 
 
 b = rbm_post('/media/chesterlab/My Passport/Files/VIC/input/dict/opstn.p', '/media/chesterlab/My Passport/Files/VIC/input/dict/rcstn.p', '/media/chesterlab/storage/post/rbm')
