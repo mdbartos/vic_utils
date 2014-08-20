a45e7e582ebd487a09dc413448f7d0da3444e827
mdbartos <matthew.d.bartos@gmail.com>
2014-08-19 17:08:33 -0700

    modified:   rbm/rbm_post.py

--- rbm/rbm_post.py
+++ rbm/rbm_post.py
@@ -2,22 +2,33 @@ import numpy as np
 import pandas as pd
 import os
 import pickle
+import ast
+from StringIO import StringIO
+import datetime
+import math
+
 
 class rbm_post():
 
-	def __init__(self, op_path, rc_path, rbm_path):
+	def __init__(self, op_path, rc_path, rbm_path, atm_path):
 		self.st_rc = pickle.load(open(rc_path, 'rb'))
 		self.st_op = pickle.load(open(op_path, 'rb'))
+		self.atm_path = atm_path
+#		self.flowpath = flowpath
 		self.st_d = {}
 		self.st_d.update({'st_rc' : self.st_rc, 'st_op' : self.st_op})
 		self.rbm_path = rbm_path
 		self.spat_d = {}
 		self.cell_d = {}
 		self.ll_d = {}
+		self.ext_wt = pd.DataFrame()
+		self.ext_atmo = pd.DataFrame()
 
 	def make_spat_d(self):
 		for fn in os.listdir(self.rbm_path):
-			spat = pd.read_fwf('%s/%s/hist.Spat' % (self.rbm_path, fn), names=['reach', 'cell', 'row', 'column', 'lat', 'lon', 'ndelta']).drop_duplicates(subset=['cell'])
+			spat = pd.read_fwf('%s/%s/hist.Spat' % (self.rbm_path, fn), names=['reach', 'cell', 'row', 'column', 'lat', 'lon', 'ndelta'])
+			spat['latlon'] = zip(spat['lat'], spat['lon'])
+			spat = spat.drop_duplicates(subset=['latlon'])
 			self.spat_d.update({fn : spat})
 
 	def make_diff(self):
@@ -44,9 +55,10 @@ class rbm_post():
 							diff = ((spat_ll[4] - stn_ll[0])**2 + (spat_ll[5] - stn_ll[1])**2)**0.5
 							cell_d.update({spat_ll[1] : diff})	
 							ll_d.update({str(tuple([spat_ll[4], spat_ll[5]])) : diff})
+		#				print k, ll_d
 						cell = min(cell_d, key=cell_d.get)
 						ll = min(ll_d, key=ll_d.get)
-						print tech, i, k[1], cell
+						#print tech, i, k[1], cell
 						mi = cell_d[cell]
 						jcell_d[k[1]] = cell
 						jll_d[k[1]] = ll
@@ -56,24 +68,54 @@ class rbm_post():
 				self.cell_d[tech] = temp_cell_d[tech]
 				self.ll_d[tech] = temp_ll_d[tech]
 
-	def read_temp(self, scen):
-		for tech in self.st_d.keys():
-			for i, k in self.cell_d[tech].items():
-				rpath = self.rbm_path + '/' + i
-				with open('%s/%s.Temp' % (rpath, scen), 'rb') as tempfile:
-					for cell in self.cell_d[tech][i].values():
-						for line in tempfile:
-							sp = line.split()
-							if sp[3] == str(cell).split('.')[0]: 
-								print line
-	
-	def get_atmos(self):
-		for i, k in self.cell_d[tech]:
-			pass
+	def make_rc_outfiles(self, scen, basin, wpath):
+		wtemp_path = self.rbm_path + '/' + basin
+		atmo_path = self.atm_path + '/' + scen
+		for pcode, cell in self.cell_d['st_rc'][basin].items():
+			with open('%s/%s.%s' % (wpath, scen, str(pcode).split('.')[0]), 'w') as outfile:
+				line_d = {}
+				with open('%s/%s.Temp' % (wtemp_path, scen), 'rb') as wtempfile:
+					line_d.update({'wtemp' : []})
+					for line in wtempfile:
+						sp = line.split()
+						if sp[3] == str(cell).split('.')[0]: 
+							line_d['wtemp'].append(line)
+				with open('%s/full_data_%s_%s' % (atmo_path, ast.literal_eval(self.ll_d['st_rc'][basin][pcode])[0], ast.literal_eval(self.ll_d['st_rc'][basin][pcode])[1])) as atmofile:
+					line_d.update({'atmo' : ''.join(atmofile.readlines())})
+#				with open('%s/%s/fluxes_%s_%s' % (flow_path, scen, self.lld[pcode][0], self.ll_d[pcode][1]) as flowfile:
+#					line_d.update('flow' : flowfile.readlines())
+#				for lin in line_d['wtemp']:
+#					print lin
+				wtemp_str = ''.join(line_d['wtemp'])
+				wtemp_str = StringIO(wtemp_str)
+				wtemp_df = pd.read_fwf(wtemp_str, header=None, names=['YEAR', 'DAY', 'REACH', 'CELL', 'SEGMENT', 'WTEMP', 'HEADTEMP', 'AIRTEMP'])
+				wtemp_df = wtemp_df.groupby('YEAR').mean()
+				wtemp_df = wtemp_df.reset_index()
+				wtemp_df['ord'] = wtemp_df.index
+				wd =  lambda x: datetime.date.fromordinal((datetime.date(1949, 1, 1)).toordinal() + int(x['ord']))
+				wtemp_df['DATE'] = wtemp_df.apply(wd, axis=1)
+				del wtemp_df['ord']
+				wtemp_df = wtemp_df.set_index('DATE')
+				print wtemp_df #groupby('DATE', sort=True).mean()
+				atmo_str = StringIO(line_d['atmo'])
+				atmo_df = pd.read_table(atmo_str, skiprows=5)
+				atmo_df = atmo_df.rename(columns = {'# YEAR': 'YEAR'})
+				self.ext_atmo = atmo_df
+				ad =  lambda x: datetime.date(int(x['YEAR']), int(x['MONTH']), int(x['DAY']))
+				atmo_df['DATE'] = atmo_df.apply(ad, axis=1)
+				atmo_df = atmo_df.set_index('DATE')
+				print atmo_df
+				cat_df = pd.concat([wtemp_df, atmo_df], join='inner', axis=1)
+				print cat_df
+#				outfile.write(str(line_d))
+				
+#	def make_st_outfiles(self, scen, basin, wpath):
 
 
-b = rbm_post('/media/chesterlab/My Passport/Files/VIC/input/dict/opstn.p', '/media/chesterlab/My Passport/Files/VIC/input/dict/rcstn.p', '/media/chesterlab/storage/post/rbm')
+b = rbm_post('/media/chesterlab/My Passport/Files/VIC/input/dict/opstn.p', '/media/chesterlab/My Passport/Files/VIC/input/dict/rcstn.p', '/media/chesterlab/storage/post/rbm', '/media/chesterlab/storage/post/st_rc')
 
 b.make_spat_d()
 
 b.make_diff()
+
+b.make_rc_outfiles('hist', 'little_col', '/media/chesterlab/storage/post/wpath') 
