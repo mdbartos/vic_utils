fdda4691d5dc98bd1a58afd1f18f2b4f762d98d5
mdbartos <matthew.d.bartos@gmail.com>
2014-11-14 16:27:46 -0700

    new file:   .wind.py.un~ 	modified:   wind.py

--- data_prep/wind.py
+++ data_prep/wind.py
@@ -31,3 +31,24 @@ df['rated_wspd'].loc[df['rated_wspd'] == 0.0] = 13.0
 df['mpower_coeff'] = 1000000*2*df['MW_turbine']/((df['rated_wspd']**3)*df['rotor_s_a']*1.2041)
 
 df = df.loc[df['mpower_coeff'] > 0.0]
+
+#########################################
+df['lat_grid'] = 0.0
+df['lon_grid'] = 0.0
+
+for i in df.index:
+
+    latcells = [int(df.loc[i, 'lat_DD']) + g for g in [0.0625*j for j in [-13,-11,-9,-7,-5,-3,-1, 1,3,5,7,9,11,13]]]
+    d = {}
+    for k in latcells:
+        d.update({df.loc[i, 'lat_DD'] - k : k})
+    minlat = d[min(d.keys())]
+    df.loc[i, 'lat_grid'] = minlat
+
+    loncells = [int(df.loc[i, 'long_DD']) + g for g in [0.0625*j for j in [-13,-11,-9,-7,-5,-3,-1, 1,3,5,7,9,11,13]]]
+    d = {}
+    for k in loncells:
+        d.update({df.loc[i, 'long_DD'] - k : k})
+    minlon = d[min(d.keys())]
+    df.loc[i, 'lon_grid'] = minlon
+
